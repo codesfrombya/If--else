081ff5332705a1b00caf67e0bc69fa0d3aa5a75e
Thamyres <thamy.beatriz05@gmail.com>
2024-10-02 07:02:24 +0000

    Pending changes exported from your codespace

---
+++ Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+     
+//if: se a condição for VERDADEIRA
+//else: se a mesma condição for FALSA
+//else if: adicionar uma nova condição, se a primeira condição for FALSA
+//switch: especificar muitos blocos de código pra serem executados
+   
+//if
+
+// com numeros
+//if (100 > 90) {
+ //System.out.println("100 is greater than 90");
+//}
+//com variáveis
+//int x = 100;
+//int y = 90;
+//if (x > y) {
+//System.out.println("x is greater than y");
+//}
+
+//else
+
+//int time = 12;
+//if (time < 11 ) {
+ //System.out.println("good morning");
+//}else {
+  //System.out.println("good afternoon");
+//}
+
+//else if
+
+//int time = 10;
+//if (time < 9 ) {
+ //   System.out.println("time to wake up!");
+//}else if( time < 11 ) {
+ //   System.out.println("good morning.");
+//}else{
+ //   System.out.println("good day.");
+//}
+
+//exemplo do cotidiano: if-else
+
+//int doorcode = 1234;
+//if (doorcode == 1234) {
+//System.out.println("Door Unlocked.");
+//} else{
+ // System.out.println("Incorrect. Try Again.");
+//}
+
+//else-if
+
+//int number = -9; 
+//if (number > 0) {
+ // System.out.println("this number is a positive number ");    
+//} else if(number < 0){
+//System.out.println("this number is a negative number");
+//} else{
+//System.out.println("this number is 0");
+//}
+
+
+
+    }
+}
+
+
+
+
